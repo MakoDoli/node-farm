74a7f26fde896e2ddb7a4a3bef98c807d95598aa
MakoDoli <makodolidze@hotmail.com>
2024-07-01 11:59:24 +0400

    add modules

--- starter/index.js
+++ starter/index.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const http = require("http");
 const path = require("path");
 const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 
 // accessing   FILE   SYSTEM
 
@@ -39,21 +40,6 @@ const url = require("url");
 
 // const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 // const dataObj = JSON.parse(data);
-
-const replaceTemplate = (template, product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-  return output;
-};
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
